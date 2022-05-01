c3d2d7b91e9ca0bf9b4f04ad1aa2d69dfce08275
Naimerith <naimerith89@gmail.com>
2022-04-30 20:14:54 -0500

    Se creo pagina 404

--- notes-now/src/app/component/modal/modal.component.ts
+++ notes-now/src/app/component/modal/modal.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { FirestoreService } from 'src/app/service/firestore.service';
 import { ObservablesService } from 'src/app/service/observables.service';
 
 
@@ -8,16 +10,28 @@ import { ObservablesService } from 'src/app/service/observables.service';
   styleUrls: ['./modal.component.scss']
 })
 export class ModalComponent implements OnInit {
+  formNote: FormGroup;
 
-  constructor(private serviceModal: ObservablesService) { }
+  constructor(private serviceModal: ObservablesService, private fb: FormBuilder, private firestore: FirestoreService) {
+    this.formNote = this.fb.group({
+      title: ['', [Validators.required, Validators.maxLength(30)]],
+      contentNote: ['', [Validators.required, Validators.maxLength(3000)]],
+    })
+  }
 
   ngOnInit(): void {
   }
 
+  /* Cerrar modal de agregar nota */
   closeModal() {
     this.serviceModal.$modal.emit(false) //Emitimos el valor false
   }
 
-  saveNote() { }
+  async AddNote() {
+    console.log(this.formNote);
+
+  }
+
+
 
 }
---
+++ notes-now/src/app/interface.ts
@@ -0,0 +1,6 @@
+export interface Data {
+    id: string,
+    title: string,
+    content: string,
+    date: number
+}
