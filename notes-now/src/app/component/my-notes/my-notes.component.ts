819dbe8a2b81096fcb58459f5c7364534db5a53e
Naimerith <naimerith89@gmail.com>
2022-06-12 18:26:45 -0500

    Se creo menu para las notas

--- notes-now/src/app/component/my-notes/my-notes.component.ts
+++ notes-now/src/app/component/my-notes/my-notes.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Interface } from '../../interface/interface';
 import { FirestoreService } from 'src/app/service/firestore.service';
+import { ObservablesService } from 'src/app/service/observables.service';
 
 @Component({
   selector: 'app-my-notes',
@@ -10,16 +11,30 @@ import { FirestoreService } from 'src/app/service/firestore.service';
 export class MyNotesComponent implements OnInit {
 
   public interface: Interface[] = []
+  modalMenu: boolean = false;
 
-  constructor(private firestore: FirestoreService) { }
+  constructor(private firestore: FirestoreService, private serviceModal: ObservablesService) { }
 
   ngOnInit(): void {
+    this.serviceModal.$modal.subscribe((valor) => { //me suscribo al servicio para observar cuando cambie de valor 
+      this.modalMenu = valor; //el parametro valor tiene el valor actual que tenga modalMenu
+    })
+
     this.firestore.getNotes().subscribe(
       (notes: any) => {
         this.interface = notes;
         console.log(notes)
       }
     );
+  }
+
+  openMenu() {
+    this.modalMenu = true;
+  }
+
+  /* Cerrar modal de agregar nota */
+  closeMenu() {
+    this.serviceModal.$modal.emit(false) //Emitimos el valor false
   }
 
 }
